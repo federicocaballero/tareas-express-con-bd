c0fc0643039386ade63511005e9bb57f2667de1c
Federico <fedecaballero1234@gmail.com>
2025-04-09 11:47:55 -0300

    instalado helmet para proteccion extra del backend

--- src/app.js
+++ src/app.js
@@ -1,39 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const taskRoutes = require('./routes/taskRoutes');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require("body-parser");
+const taskRoutes = require("./routes/taskRoutes");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const errorHandler = require("./middleware/errorHandler");
+const { default: helmet } = require("helmet");
 dotenv.config();
 const app = express();
 
+app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use('/tasks', taskRoutes);
-app.use(morgan('dev'));
-const pool = require('./config/db');
+app.use("/tasks", taskRoutes); // Define la ruta base para las tareas
+app.use(errorHandler);
+app.use(helmet());
+const pool = require("./config/db");
 
 const PORT = process.env.DB_PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
-})
+});
 
-
-pool.connect((err, client, release)=>{
+pool.connect((err, client, release) => {
   if (err) {
-    console.error('Error al conectar con la base de datos:', err.stack);
+    console.error("Error al conectar con la base de datos:", err.stack);
     return;
   }
-  console.log('✅ Conexión exitosa a la base de datos');
+  console.log("✅ Conexión exitosa a la base de datos");
   release();
-})
-
-// async function testDB() {
-//   try {
-//     const res = await pool.query('SELECT NOW()');
-//     console.log('✅ Conexión exitosa a la base de datos:', res.rows[0]);
-//   } catch (err) {
-//     console.error('❌ Error al conectar con la base de datos:', err);
-//   }
-// }
-
-// testDB();
+});
